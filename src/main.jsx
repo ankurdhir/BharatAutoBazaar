b6b136bb9890a25386b1f71c3149fe30f1a9aafc
Ankur Dhir <adhir@adobe.com>
2025-08-17 15:27:39 +0530

    Router: use basename from import.meta.env.BASE_URL for GitHub Pages subpath routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter future={{
+    <BrowserRouter basename={import.meta.env.BASE_URL} future={{
       v7_startTransition: true,
       v7_relativeSplatPath: true
     }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-) 
+)
